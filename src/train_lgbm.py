6bd4ab0541e1949f8160381be03ffa8a2ec5e4f9
Маряхин Никита Романович <nrmaryakhin@MacBook-Air-Marahin.local>
2026-09-03 11:42:53 +0300

    Train and saving final model

--- src/train_lgbm.py
+++ src/train_lgbm.py
@@ -1,8 +1,19 @@
 from lightgbm import LGBMClassifier
 import preprocessing as pp
+import joblib
 
 
 
 
-model = LGBMClassifier(random_state=42, class_weight='balanced')
+model = LGBMClassifier(
+    n_estimators=1100,
+    num_leaves=31,
+    learning_rate=0.02,
+    min_child_samples=20,
+    reg_lambda=0,
+    random_state=42,
+    class_weight="balanced"
+)
+
 model.fit(pp.X_train, pp.y_train)
+joblib.dump(model, '../models/lgbm_model.pkl')
